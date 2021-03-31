bab9e53827b37b1324dda94a327f590022305312
Heriberto Montoya <hmontoya3821@gmail.com>
2021-03-31 10:40:54 -0500

    back button now works

--- frontend/src/components/PhotoDetailPage/Image.js
+++ frontend/src/components/PhotoDetailPage/Image.js
@@ -1,24 +1,36 @@
 import React from "react";
 import styles from "./PhotoDetail.module.css";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+import { useHistory } from "react-router";
 
+function Image({ src, alt, nextPhotoId, prevPhotoId }) {
+  const history = useHistory();
 
-function Image({src, alt}) {
+  const goBack = () => {
+    history.goBack();
+  };
+
   return (
     <div className={styles.heightController}>
-      <NavLink to='/explore' className={styles.backBtn}>
+      <NavLink to="/explore" className={styles.backBtn}>
         <i className={`fas fa-long-arrow-alt-left ${styles.leftBackArrow}`}></i>
         Back to explore
       </NavLink>
-      <NavLink to='#' className={styles.leftPhotoContainer}>
+      {/* <Link to={`/photos/${prevPhotoId}`} className={styles.leftPhotoContainer}>
         <i className={`fas fa-angle-left ${styles.leftArrow}`}></i>
-      </NavLink>
+      </Link> */}
+      <button onClick={goBack} className={styles.leftPhotoContainer}>
+        <i className={`fas fa-angle-left ${styles.leftArrow}`}></i>
+      </button>
       <div className={styles.imageContainer}>
         <img className={styles.imageContainer_image} src={src} alt={alt}></img>
       </div>
-      <NavLink to='#' className={styles.rightPhotoContainer}>
+      <Link
+        to={`/photos/${nextPhotoId}`}
+        className={styles.rightPhotoContainer}
+      >
         <i className={`fas fa-angle-right ${styles.rightArrow}`}></i>
-      </NavLink>
+      </Link>
     </div>
   );
 }
--- frontend/src/components/PhotoDetailPage/index.js
+++ frontend/src/components/PhotoDetailPage/index.js
@@ -1,25 +1,38 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router";
-import { getImageDetail } from "../../store/photos";
+import { useHistory, useParams } from "react-router";
+import { getImageDetail, getPhotos } from "../../store/photos";
 import Image from './Image'
 import ImageDetail from './ImageDetail'
 
 
 function PhotoDetailPage() {
   const dispatch = useDispatch();
-
+  const history = useHistory();
   const {photoId} = useParams();
+
+  const photos = useSelector((state) => {
+    return state.photo.list?.map((photo) => photo);
+  });
 
   const photo = useSelector((state) => state.photo.currentPhoto);
 
+  const nextPhoto = photos?.find(pic => pic?.id > photo?.id )
+  const prevPhoto = photos?.find(pic => pic?.id < photo?.id )
+  // prevPhotoId={prevPhoto?.id}
+  // const goBack = () => {
+  //   history.goBack()
+  // }
+  // goBack={goBack}
+
   useEffect(() => {
     dispatch(getImageDetail(photoId));
-  }, [dispatch]);
+    dispatch(getPhotos());
+  }, [dispatch, photoId]);
 
   return (
     <div>
-      <Image src={photo?.photo_url} alt={photo?.title}/>
+      <Image src={photo?.photo_url} alt={photo?.title} nextPhotoId={nextPhoto?.id} prevPhotoId={prevPhoto?.id}/>
       <ImageDetail userId={photo?.userId} title={photo?.title} description={photo?.description}/>
     </div>
   )
