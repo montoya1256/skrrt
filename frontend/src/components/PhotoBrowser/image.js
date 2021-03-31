f3dd06cdf670f1bd251efdb47f747bc0ab50ea5a
Heriberto Montoya <hmontoya3821@gmail.com>
2021-03-30 21:06:20 -0500

    when picture is clicked in explore page it will now take you to that pictures page

--- frontend/src/components/PhotoBrowser/image.js
+++ frontend/src/components/PhotoBrowser/image.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import { NavLink } from "react-router-dom";
+
 import styles from "./PhotoBrowser.module.css";
 
-function Image({src, alt}) {
+function Image({src, alt, id}) {
   return (
-    <div>
+    <NavLink to={`/photos/${id}`}>
       <img className={styles.singleImage} src={src} alt={alt} ></img>
-    </div>
+    </NavLink>
   )
 }
 
